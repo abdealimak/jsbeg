5232c7dbd04d9728a39255fa74190e5a3753dca2
Abdeali Makda <abdealimac@gmail.com>
2026-01-20 23:49:35 +0530

    Add files via upload

---
+++ tea.js
@@ -0,0 +1,15 @@
+let choice="tea";
+
+switch (choice){
+    case "tea":
+        console.log("₹10")
+        break;
+    case "coffee":
+        console.log("₹20")
+        break;
+    case "ganne ka juice":
+        console.log("₹30")
+        break;
+    default:
+        console.log("items not available")
+}
