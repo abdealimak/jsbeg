5a36cd34daba653b08fb5393a9af4373ae1b9aa5
Abdeali Makda <abdealimac@gmail.com>
2026-02-05 20:42:24 +0530

    Update async.js

--- async/async.js
+++ async/async.js
@@ -1,1 +1,13 @@
+// METHOD 1: fetch() with .then()
 
+fetch("api url")
+      .then(response => response.json())
+      .then(data => console.log(data));
+
+// METHOD 2: async / await
+
+async function getData(){
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json();
+    console.log(data);
+}
